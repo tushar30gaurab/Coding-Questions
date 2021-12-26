2591d2a472e50364609eeb39554758f2359445c0
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-12-26 12:18:20 +0530

    Time: 276 ms (92.11%), Space: 0B (100.00%) - LeetHub

--- nth-highest-salary/nth-highest-salary.sql
+++ nth-highest-salary/nth-highest-salary.sql
@@ -4,7 +4,7 @@ DECLARE M INT;
 SET M = N-1;
   RETURN (
       # Write your MySQL query statement below.
-       SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M,1
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M,1
       
   );
 END
