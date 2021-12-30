91414bdd22eb1b57910cd5a8bbafef1d6f6e11f6
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-12-30 21:37:40 +0530

    Update two-sum.java

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,3 +1,4 @@
+//Solution using ArrayList
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         List<Integer> list = new ArrayList<Integer>();
@@ -12,5 +13,26 @@ class Solution {
                 return new int[]{i, list.indexOf(complement)};
         }
         return new int[]{};
+    }
+}
+
+//Solution using HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int complement = target - nums[i];
+            if(hm.containsKey(complement))
+                return new int[]{hm.get(complement), i};
+            else
+                hm.put(nums[i], i);
+            
+        }
+        
+        return new int[]{-1,-1};
+        
     }
 }
