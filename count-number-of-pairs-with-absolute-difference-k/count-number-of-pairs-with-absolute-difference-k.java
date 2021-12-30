350d02958b31ce3d97808890d612f542f9ab5b16
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2021-12-30 22:46:26 +0530

    Update count-number-of-pairs-with-absolute-difference-k.java

--- count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,3 +1,4 @@
+// O(n^2) solution - Brute Force Method
 class Solution {
     public int countKDifference(int[] nums, int k) {
         
@@ -9,6 +10,27 @@ class Solution {
                     count++;
             }
         }
+        return count;
+    }
+}
+
+// O(N) Solution - Count Sort Technique
+
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        
+        int count=0;
+        int n = nums.length;
+        int arr[] = new int[201]; //taking advantage of leetcode constraint
+        
+        for(int i=0; i<n;i++){
+            arr[nums[i]]++;
+        }
+        
+        for(int i=k+1; i<201; i++){
+            count += arr[i]*arr[i-k]; // main step - pay attention!
+        }
+        
         return count;
     }
 }
