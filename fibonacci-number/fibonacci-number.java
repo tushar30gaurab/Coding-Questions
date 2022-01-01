a5ade9eb6299fc518c8a18616d7cb1811001c211
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-01 21:38:53 +0530

    Update fibonacci-number.java

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,3 +1,4 @@
+// 1. Simple recursive solution | Time Complexity: O(2^n) | Space complexity: O(1)
 class Solution {
     public int fib(int n) {
         
@@ -10,6 +11,76 @@ class Solution {
         
         int biggerSolution = firstSmallerSolution + secondSmallerSolution;
         return biggerSolution;
+        
+    }
+}
+
+// 2.(a) Dynamic Programming solution(Recursive) | Time Complexity: O(n) | Space complexity: O(n)
+import java.util.*;
+class Solution {
+    public int fib(int n) {
+        
+        int arr[] = new int[n+1];
+        Arrays.fill(arr, -1);
+        return calculateFib(n, arr);
+        
+    }
+    
+    public int calculateFib(int n, int arr[]){
+        
+        if(n==0 || n==1){
+            arr[n] = n;
+            return n;
+        }
+        
+        if(arr[n]!= -1){
+            return arr[n];
+        }
+        
+        return arr[n] = calculateFib(n-1, arr) + calculateFib(n-2, arr);
+    }
+}
+
+// 2.(b) Dynamic Programming solution(Iterative) | Time Complexity: O(n) | Space complexity: O(n)
+class Solution {
+    public int fib(int n) {
+        
+        if(n==0 || n==1){
+            return n;
+        }
+        
+        int arr[] = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return arr[n];
+    }
+}
+
+// 3. Advanced Dynamic Programming Solution(Iterative) | Time Complexity: O(n) | Space complexity: O(1)
+class Solution {
+    public int fib(int n) {
+        
+        if(n==0 || n==1){
+            return n;
+        }
+        
+        int first = 0;
+        int second = 1;
+        int sum = 0;
+        
+        for(int i=2; i<=n; i++){
+            sum = first + second;
+            first = second;
+            second = sum;
+            
+        }
+        
+        return sum;
         
     }
 }
