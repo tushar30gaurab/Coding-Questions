9166f902a38bd7f478e9e4e4740c92755e5ee314
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-02 12:16:58 +0530

    Update power-of-two.java

--- power-of-two/power-of-two.java
+++ power-of-two/power-of-two.java
@@ -1,3 +1,18 @@
+//Iterative Approach
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        long i,j;
+        j=n;
+        for(i=0;i<=31;i++)
+        {
+            if(j==(long)Math.pow(2,i))
+                return true;
+        }
+        return false;
+    }
+}
+
+//Recursive Approach
 class Solution {
     
     public boolean checkPower(int n, int counter){
