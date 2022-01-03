b5d241cfff7f41ce3c38f956891becb1c8479be0
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-03 18:37:05 +0530

    Update number-of-1-bits.java

--- number-of-1-bits/number-of-1-bits.java
+++ number-of-1-bits/number-of-1-bits.java
@@ -1,3 +1,4 @@
+// Approach 1
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
@@ -7,6 +8,20 @@ public class Solution {
             n = n>>>1;
         }
         
+        return count;
+    }
+}
+
+
+//Approach 2
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        int count = 0;
+        for(int i=0;i<32;i++){
+            count += (n>>i & 1) == 1 ? 1 : 0;
+        }
         return count;
     }
 }
