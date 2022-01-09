986bf3959c203ef5237eb60cc7347a98a72bd7f8
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-09 18:39:30 +0530

    Update 897-increasing-order-search-tree.java

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -1,3 +1,34 @@
+// Iterative solution
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        if(root == null) return null;
+        
+        ArrayList<TreeNode> list = new ArrayList<>();
+        inorderBST(root, list);                             // perform inorder traversal & store each node in a space (arraylist in this case). 
+        int i=0;
+        for( ; i<list.size()-1; i++){
+            list.get(i).right = list.get(i+1);               //setting every node's right as next node from inorder fashion
+            list.get(i).left = null;                         //setting every node's left as null
+        }
+        
+        list.get(list.size()-1).left = null;                // leaf node's left = null
+        list.get(list.size()-1).right = null;               // leaf node's right = null
+        
+        return list.get(0);              // return root node
+    }
+    
+    public void inorderBST(TreeNode root, ArrayList<TreeNode> list){
+        if(root == null)    // base condition
+            return;
+        
+        inorderBST(root.left, list);
+        list.add(root);
+        inorderBST(root.right, list);
+    }
+}
+
+
+// Recursive Solution
 class Solution {
     
     TreeNode pathfinder; // for finding path & connecting all dots
