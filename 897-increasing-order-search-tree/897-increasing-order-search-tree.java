88be79400b720f7b534adee9eb83463390075954
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-09 18:32:03 +0530

    Time: 0 ms (100.00%), Space: 38.2 MB (28.76%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -1,27 +1,26 @@
 class Solution {
+    
+    TreeNode pathfinder; // for finding path & connecting all dots
     public TreeNode increasingBST(TreeNode root) {
-        if(root == null) return null;
         
-        ArrayList<TreeNode> list = new ArrayList<>();
-        inorderBST(root, list); // customised function for performing Inorder Traversal
-        int i=0;
-        for( ; i<list.size()-1; i++){
-            list.get(i).right = list.get(i+1);
-            list.get(i).left = null;
-        }
+        TreeNode dummy = new TreeNode(0); // dummy node for starting reference.
+        pathfinder = dummy;
         
-        list.get(list.size()-1).left = null;
-        list.get(list.size()-1).right = null;
-        
-        return list.get(0);
+        inorder(root); // actual work done here.
+        return dummy.right; // return right node of dummy node (the actual root node).
     }
     
-    public void inorderBST(TreeNode root, ArrayList<TreeNode> list){
-        if(root == null)
+    public void inorder(TreeNode root){
+        
+        if(root == null)  // base condition
             return;
         
-        inorderBST(root.left, list);
-        list.add(root);
-        inorderBST(root.right, list);
+        inorder(root.left); // call for left subtree (assume it will do the work for left subtree)
+        
+        root.left = null; //  making root left node as null
+        pathfinder.right = root; //  pathfinder's right part will now point the root node.
+        pathfinder = root; // pathfinder node pointing one node ahead(on next function call, this will be the left subtree).
+        
+        inorder(root.right);  // call for right subtree (assume it will do the work for right subtree)
     }
 }
