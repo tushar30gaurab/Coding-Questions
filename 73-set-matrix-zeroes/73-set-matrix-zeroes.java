fe377531e59809552721921b8c6f6ff03ee6968b
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-01-11 23:11:44 +0530

    Update 73-set-matrix-zeroes.java

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,3 +1,4 @@
+// Time complexity O(m x n) | Space Complexity O(m + n)
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
@@ -21,6 +22,51 @@ class Solution {
                 if(row.contains(i) || col.contains(j)){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+    }
+}
+
+
+// Space optimised approach - Time Complexity O(m x n) | Space Complexity O(1)
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean isZero = false;
+        
+        for(int i=0; i<m; i++){
+            
+            if(matrix[i][0] == 0){
+                isZero = true;
+            }
+            
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(isZero){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
     }
