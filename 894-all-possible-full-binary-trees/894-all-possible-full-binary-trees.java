702d43d7411558927e49db22acdaaab6d8a6a562
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2022-04-10 20:46:53 +0530

    Time: 7 ms (36.07%), Space: 60.6 MB (24.73%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
@@ -1,18 +1,25 @@
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
-        
+        Map<Integer, List<TreeNode>> hmap = new HashMap<>();
         List<TreeNode> result = new ArrayList<>();
-        if(n==1){
+        
+        if(n%2 == 0)
+            return result;
+        
+        if(n == 1){
             result.add(new TreeNode(0));
             return result;
         }
+        
+        if(hmap.containsKey(n))
+            return hmap.get(n);
         
         for(int i=1; i<n; i+=2){
             List<TreeNode> left = allPossibleFBT(i);
             List<TreeNode> right = allPossibleFBT(n-i-1);
             
-            for(TreeNode l: left){
-                for(TreeNode r: right){
+            for(TreeNode l:left){
+                for(TreeNode r:right){
                     TreeNode root = new TreeNode(0);
                     root.left = l;
                     root.right = r;
@@ -20,7 +27,13 @@ class Solution {
                 }
             }
         }
-        
         return result;
     }
 }
+
+
+
+
+
+
+
