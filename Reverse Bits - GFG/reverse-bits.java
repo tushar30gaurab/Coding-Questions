bccfd0ee7d7584838dbd5056e8ad7a7ec8f15d7e
Tushar Gourav <69919283+tushar30gaurab@users.noreply.github.com>
2023-06-22 11:30:10 +0530

    Added solution - LeetHub

---
+++ Reverse Bits - GFG/reverse-bits.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long X = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.reversedBits(X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Long reversedBits(Long X) {
+        // code here
+        
+        long reverse = 0;
+        int k=1;
+        
+        while(k<=32){
+            reverse = reverse *2 + (X%2);
+            X = X/2;
+            k++;
+        }
+        
+        return reverse;
+    }
+};
